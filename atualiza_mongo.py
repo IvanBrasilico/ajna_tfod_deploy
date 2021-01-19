9ca4dd012fdfd3d81452955ef6cf7bb9fe668c9b
IvanBrasilico <brasilico.ivan@gmail.com>
2021-01-19 17:04:44 -0300

    Conexão a banco diferente de teste e mostrar _id e predictions no log

--- atualiza_mongo.py
+++ atualiza_mongo.py
@@ -68,8 +68,10 @@ def update_mongo(model, db, limit=10):
 
 if __name__ == '__main__':
     MONGODB_URI = os.environ.get('MONGODB_URI')
+    database = ''.join(MONGODB_URI.rsplit('/')[-1:])
     if not MONGODB_URI:
         MONGODB_URI = 'mongodb://localhost'
-    conn = MongoClient(host=MONGODB_URI)
-    mongodb = conn['test']
-    update_mongo(mongodb, 60000)
+        database = 'test'
+    with MongoClient(host=MONGODB_URI) as conn:
+        mongodb = conn[database]
+        update_mongo(mongodb, 60000)
