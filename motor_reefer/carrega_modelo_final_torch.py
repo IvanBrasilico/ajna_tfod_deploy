eb0c87887a65a1942c3e3839c3bfc25716b1d764
atejap05 <atejap05@yahoo.com.br>
2021-04-12 21:02:51 -0400

    refatora classe Detectron2Model para 2 classes

--- motor_reefer/carrega_modelo_final_torch.py
+++ motor_reefer/carrega_modelo_final_torch.py
@@ -1,5 +1,5 @@
+from posix import O_ACCMODE
 import time
-import torch
 # device = torch.device('cpu')
 from detectron2.utils.logger import setup_logger
 
@@ -11,26 +11,28 @@ from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog
 
+import matplotlib.pyplot as plt
+from collections import defaultdict
 import numpy as np
 from PIL import Image
 import os, cv2
 
-# IMG_SIZE = 380
-MODEL = 'models/detectron2_fastcnn/model_final_ciclo03.pth'
 
+MODEL = 'models/detectron2_fastcnn/model_final_ciclo03.pth'
+MODEL_2CLASSES_PATH = '/home/jap05/Projects/ajna_tfod_deploy/motor_reefer/checkpoints/model_final_ciclo02.pth'
 
 class Detectron2Model():
     """[summary]
     """
 
-    def __init__(self, model_path=MODEL, num_classes=1, classes_names=['motor']):
+    def __init__(self, model_path, num_classes, classes_names):
         self.model_path = model_path
         self.num_classes = num_classes
         self.classes_names = classes_names
         self.threshold = .8
         self.cfg = get_cfg()
         # self.cfg.MODEL.DEVICE = 'cpu'
-        self.predictor = self.get_predictor()
+        #self.predictor = self.get_predictor()
 
     def set_threshold(self, threshold):
         """[summary]
@@ -40,24 +42,23 @@ class Detectron2Model():
         """
         self.threshold = threshold
 
-    def set_model_zoo(self):
-        pass
 
     def get_predictor(self):
-        # cfg = get_cfg()
+
         # Must be same as model trained in.
         self.cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml"))
         # Path to saved model weights (trained model)
         self.cfg.MODEL.WEIGHTS = self.model_path
         # if using datasets for metadata
-        # cfg.DATASETS.TEST = ('my_dataset_test', )
+        self.cfg.DATASETS.TEST = ('my_dataset_test', )
         self.cfg.MODEL.ROI_HEADS.NUM_CLASSES = self.num_classes
         # Threshhold to show predictions
         self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = self.threshold
         # create predictor instance to use
         return DefaultPredictor(self.cfg)
 
-    def predict(self, image):
+    @staticmethod
+    def get_preds(predictor: object, image_path: str):
         """[summary]
 
         Args:
@@ -66,29 +67,21 @@ class Detectron2Model():
         Returns:
             [type]: [description]
         """
-
-        return self.predictor(image)
-
-    def get_preds(self, image):
-        """[summary]
-
-        Args:
-            image ([type]): [description]
-
-        Returns:
-            [type]: [description]
-        """
+        image = cv2.imread(image_path)
 
         pred_boxes, pred_classes, pred_scores = list(), list(), list()
 
-        predictions = self.predict(image)['instances'].to('cpu')
-        pred_fields = predictions.get_fields()
+        predictions = predictor(image)['instances'].to('cpu')
 
-        pred_boxes = [tensor.tolist() for tensor in list(pred_fields['pred_boxes'])]
-        pred_classes = pred_fields['pred_classes'].tolist()
-        pred_scores = pred_fields['scores'].tolist()
+        if predictions:
+                
+            pred_fields = predictions.get_fields()
+            
+            pred_boxes = [tensor.tolist() for tensor in list(pred_fields['pred_boxes'])]
+            pred_classes = pred_fields['pred_classes'].tolist()
+            pred_scores = pred_fields['scores'].tolist()
 
-        return pred_boxes, pred_classes, pred_scores
+            return pred_boxes, pred_classes, pred_scores
 
     def plot_detections(self, image):
 
@@ -102,7 +95,7 @@ class Detectron2Model():
 
         v = Visualizer(image[:, :, ::-1], metadata=test_metadata, scale=0.4)
         out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-        cv2_imshow(out.get_image()[:, :, ::-1])
+        plt.imshow(out.get_image()[:, :, ::-1])
 
     def crop(self, image_path: str, output_path: str = None):
         """[summary]
@@ -115,45 +108,84 @@ class Detectron2Model():
             [type]: [description]
         """
 
-        pred_boxes, pred_classes, _ = self.get_preds(image_path)
-        img = Image.open(image_path)
+        try:
+            pred_boxes, pred_classes, _ = self.get_preds(image_path)
+        
+           
+            img = Image.open(image_path)
 
-        cropped_images = defaultdict(list)
+            cropped_images = defaultdict(list)
 
-        for bbox, classe in zip(pred_boxes, pred_classes):
-            crop = img.crop((bbox[0], bbox[1], bbox[2], bbox[3]))
-            cropped_images[classe].append(crop)
+            for bbox, classe in zip(pred_boxes, pred_classes):
+                crop = img.crop((bbox[0], bbox[1], bbox[2], bbox[3]))
+                cropped_images[classe].append(crop)
 
-        if output_path:
-            image_name, ext = os.path.splitext(os.path.basename(image_path))
-
-            if not os.path.exists(output_path):
-                os.mkdir(output_path)
-            for classe, bboxes in cropped_images.items():
-                classe_folder = os.path.join(output_path, str(classe))
-                if not os.path.exists(classe_folder):
-                    os.mkdir(classe_folder)
+            if output_path:
+                image_name, ext = os.path.splitext(os.path.basename(image_path))
+        
+                if not os.path.exists(output_path):
+                    os.mkdir(output_path)
+                for classe, bboxes  in cropped_images.items():
+                    classe_folder = os.path.join(output_path, str(classe))
+                    if not os.path.exists(classe_folder):
+                        os.mkdir(classe_folder)
 
                     for bbox in bboxes:
                         bbox.save(os.path.join(classe_folder, image_name + str(ext)))
 
-        return [np.asarray(el) for crop in cropped_images.values() for el in crop][0]
+            crops = [np.asarray(el) for crop in cropped_images.values() for el in crop]
+
+            return crops[0] if len(crops) > 0 else crops
+        
+        except TypeError:
+            
+            print(f"No preds for image {image_path.split('/')[-1]}. Try a threshold less then {self.threshold}")
 
 
 # TODO fazer deploy em Flask
 
 if __name__ == '__main__':
+    
+    def do_contaminado(predictor, image_path):
+
+        for filename in os.listdir(image_path):
+
+            preds = model.get_preds(predictor, os.path.join(image_path, filename))
+        
+            if preds:
+
+                pred_boxes, pred_classes, pred_scores = preds
+                if pred_classes[0] == 0:
+                    print('')
+                    print(f'Motor Limpo!')
+                    print(f'coordenadas: {pred_boxes[0]}')
+                    print(f'Score: {pred_scores[0]:.2f}')
+
+                else:
+                    print('')
+                    print(f'>>>>Motor Contaminado!<<<<<')
+                    print(f'coordenadas: {pred_boxes[0]}')
+                    print(f'Score: {pred_scores[0]:.2f}')               
+
+            else:
+                print('')
+                print(f'No preds for image: {filename} with threshold {model.threshold}')
+
+    
+    ##################### 1 Classe ####################################################
+    """
     saved_model_path = MODEL
     num_classes = 1
     classes_names = ['motor']
 
     s = time.time()
+    
     model = Detectron2Model(
         model_path=saved_model_path,
         num_classes=num_classes,
         classes_names=classes_names
     )
-
+    
     ground_true_bbox = [[122, 21, 210, 637],
                         [122, 21, 210, 637]]
     test_images = ['test/motor_somente_imgs/5f7b12cccccffe00323542c0.jpg',
@@ -173,6 +205,25 @@ if __name__ == '__main__':
         print(f'{s1 - s0} segundos para predição')
         assert sum([abs(item_pred - item_groung_truth)
                     for item_pred, item_groung_truth in zip(pred_boxes[0], ground_true_bbox[ind])]) < 24
+    """
+   #####################  2Classes ###################################################
 
+    saved_model_path = [PATH_TO_SAVED_MODEL]
+    num_classes = 2
+    classes_names = ['vazio', 'nvazio']
 
+    model = Detectron2Model(
+
+        model_path=saved_model_path,
+        num_classes=num_classes,
+        classes_names=classes_names
+    )
+    
+    model.set_threshold(.6)
+
+    images_path = [PATH_TO_IMAGES]
+
+    predictor = model.get_predictor()
+
+    do_contaminado(predictor, images_path)
 
