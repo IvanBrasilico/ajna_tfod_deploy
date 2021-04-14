472babe556cd20fd5671303a181e69dbdda3d174
IvanBrasilico <brasilico.ivan@gmail.com>
2021-04-14 18:24:07 -0300

    Publicação modelo efficientnet de reefer contaminado

---
+++ reefer_contaminado/atualiza_mongo_rc.py
@@ -0,0 +1,64 @@
+import io
+import logging
+import os
+import sys
+import time
+from PIL import Image
+from bson import ObjectId
+from collections import Counter
+from datetime import datetime
+from gridfs import GridFS
+from pymongo import MongoClient
+
+logging.basicConfig(level=logging.DEBUG)
+
+MIN_RATIO = 2.1
+
+
+def monta_filtro(db, limit: int):
+    filtro = {'metadata.contentType': 'image/jpeg',
+              'metadata.dataescaneamento': {'$gte': datetime(2020, 12, 16)},
+              'metadata.predictions.reefer.reefer_bbox': {'$exists': True},
+              'metadata.predictions.reefer.reefer_contaminado': {'$exists': False}}
+    cursor = db['fs.files'].find(
+        filtro, {'metadata.predictions': 1}).limit(limit)[:limit]
+    logging.info('Consulta ao banco efetuada.')
+    return cursor
+
+
+def update_mongo(model, db, limit=10):
+    fs = GridFS(db)
+    cursor = monta_filtro(db, limit)
+    counter = Counter()
+    for ind, registro in enumerate(cursor):
+        s0 = time.time()
+        _id = ObjectId(registro['_id'])
+        # pred_gravado = registro.get('metadata').get('predictions')
+        grid_out = fs.get(_id)
+        image = grid_out.read()
+        pil_image = Image.open(io.BytesIO(image))
+        pil_image = pil_image.convert('RGB')
+        s1 = time.time()
+        logging.info(f'Elapsed retrieve time {s1 - s0}')
+        pred = model.predict(pil_image)
+        s2 = time.time()
+        logging.info(f'Elapsed model time {s2 - s1}.')
+        logging.info({'_id': _id, 'vazio': pred})
+        db['fs.files'].update(
+            {'_id': _id},
+            {'$set': {'metadata.predictions.0.reefer.reefer_contaminado': pred}}
+        )
+        s3 = time.time()
+        logging.info(f'Elapsed update time {s3 - s2} - registro {ind}')
+
+
+if __name__ == '__main__':
+    model = ModelContaminado()
+    MONGODB_URI = os.environ.get('MONGODB_URI')
+    database = ''.join(MONGODB_URI.rsplit('/')[-1:])
+    if not MONGODB_URI:
+        MONGODB_URI = 'mongodb://localhost'
+        database = 'test'
+    with MongoClient(host=MONGODB_URI) as conn:
+        mongodb = conn[database]
+        update_mongo(model, mongodb, 10)
---
+++ reefer_contaminado/carrega_modelo_final_rc.py
@@ -0,0 +1,77 @@
+# TODO: Descomentar abaixo para rodar inferência na CPU
+# os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+import tensorflow as tf
+
+# TODO: Para tensorflow não comer toda a memória
+try:
+    physical_devices = tf.config.experimental.list_physical_devices('GPU')
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+except:
+    pass
+
+import numpy as np
+from PIL import Image
+from tensorflow.keras.applications import EfficientNetB4
+from tensorflow.keras import layers
+
+
+IMG_SIZE = 380
+MODEL = 'models/efficientnetb4/contaminados_ciclo0.h5'
+
+
+def build_model():
+    # Carregar modelo e pesos do modelo
+    inputs = layers.Input(shape=(IMG_SIZE, IMG_SIZE, 3))
+    model = EfficientNetB4(include_top=False, input_tensor=inputs)
+    # Rebuild top
+    x = layers.GlobalAveragePooling2D(name="avg_pool")(model.output)
+    x = layers.BatchNormalization()(x)
+    top_dropout_rate = 0.2
+    x = layers.Dropout(top_dropout_rate, name="top_dropout")(x)
+    outputs = layers.Dense(1, activation="sigmoid", name="pred")(x)
+    model = tf.keras.Model(inputs, outputs, name="EfficientNet")
+    model.trainable = False
+    model.load_weights(MODEL)
+    return model
+
+
+class ModelContaminado():
+    def __init__(self):
+        self.model = build_model()
+
+    def image_to_np(self, image):
+        image = image.resize((IMG_SIZE, IMG_SIZE), Image.LANCZOS)
+        image_np = np.array(image)
+        return np.expand_dims(image_np, axis=0)
+
+    def predict(self, image):
+        img_array = self.image_to_np(image)
+        prediction = self.model.predict(img_array)
+        # Retorna True se contaminado e False se não contaminado
+        return float(prediction[0]) > .5
+
+
+classes = {0: 'Nao contaminado',
+           1: 'Contaminado'}
+
+if __name__ == '__main__':
+    model = ModelContaminado()
+
+    def image_test(path, filename):
+        pil_image = Image.open(path)
+        pil_image = pil_image.convert('RGB')
+        pred = model.predict(pil_image)
+        print(f'Resultado de Image {path}: {pred}')
+        return pred
+
+
+    test_images = ['tests/HLXU6772322 A.jpg',
+                   'tests/TEMU9131666 B.jpg',
+                   'tests/60180ede0be94217a2cf91d5.jpg',
+                   'tests/60180d8d0be94217a2cf6b47.jpg'
+                   ]
+    ground_true = [True, True, False, False]
+    for ind, path in enumerate(test_images):
+        pred = image_test(path, f'teste{ind}.jpg')
+        # print(pred, )
+        assert ground_true[ind] == pred
