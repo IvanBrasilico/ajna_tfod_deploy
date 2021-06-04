26704f030a8c5618aefd2fcd71649ee70cdb3fc7
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2021-06-03 20:23:12 -0400

    teste 6

--- reefer_contaminado/atualiza_mongo_rc.py
+++ reefer_contaminado/atualiza_mongo_rc.py
@@ -164,7 +164,8 @@ if __name__ == '__main__':
         # a linha abaixo.
         # baixa_falso_positivo(comunica, limit)
         
-        comunica.get_metrics(fbeta=0.942, take=1000)
+        comunica.get_metrics(fbeta=0.951, take=1000)
+
 
 
 #threshold = .6
